f2994e63d8b8cf4bed0b7108fd3e1b76479b18a8
Tshepo <rocboyt@gmail.com>
2019-09-16 12:01:33 +0200

    Refactored models

--- app/src/main/java/co/za/dvt/myskilldevapp/models/Car.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/models/Car.kt
@@ -1,9 +1,16 @@
 package co.za.dvt.myskilldevapp.models
 
+import com.google.gson.annotations.SerializedName
+
 class Car {
+    @SerializedName("brand")
     var brand:String? = null
+    @SerializedName("model")
     var model:String? = null
+    @SerializedName("year")
     var year:String? = null
+    @SerializedName("color")
     var color:String? = null
+    @SerializedName("pic")
     var pic:String? = null
 }
--- app/src/main/java/co/za/dvt/myskilldevapp/models/Roll.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/models/Roll.kt
@@ -1,6 +1,10 @@
 package co.za.dvt.myskilldevapp.models
 
+import com.google.gson.annotations.SerializedName
+
 class Roll {
+    @SerializedName("attempt")
     var attempt:Int = 0
+    @SerializedName("luckyNumber")
     var luckyNumber:Int = 0
 }
