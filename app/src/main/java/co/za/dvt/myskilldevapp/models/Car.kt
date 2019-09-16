f1d5314914f36e67c30082866f77626bd456ce4e
Tshepo <rocboyt@gmail.com>
2019-09-16 10:45:14 +0200

    Added car and roll model

---
+++ app/src/main/java/co/za/dvt/myskilldevapp/models/Car.kt
@@ -0,0 +1,9 @@
+package co.za.dvt.myskilldevapp.models
+
+class Car {
+    var brand:String? = null
+    var model:String? = null
+    var year:String? = null
+    var color:String? = null
+    var pic:String? = null
+}
---
+++ app/src/main/java/co/za/dvt/myskilldevapp/models/Roll.kt
@@ -0,0 +1,6 @@
+package co.za.dvt.myskilldevapp.models
+
+class Roll {
+    var attempt:Int = 0
+    var luckyNumber:Int = 0
+}
