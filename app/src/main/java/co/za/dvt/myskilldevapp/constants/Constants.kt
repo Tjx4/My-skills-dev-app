341319cff077f880cb6cfcddaf16f34da82a50b3
Tshepo <rocboyt@gmail.com>
2019-09-17 13:51:07 +0200

    Fixed constant string bug

--- app/src/main/java/co/za/dvt/myskilldevapp/constants/Constants.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/constants/Constants.kt
@@ -1,6 +1,6 @@
 package co.za.dvt.myskilldevapp.constants
 
 val HOST = "http://appic.co.za/"
-const val GET_LUCKY_NUMBER = "api/currentLuckyNumber.php"
+const val GET_LUCKY_NUMBER = "api/luckyNumber.php"
 const val GET_AVAILABLE_CARS = "api/cars.php"
 const val ATMT = "attempt"
