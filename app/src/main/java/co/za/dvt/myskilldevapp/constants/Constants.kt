1aa1ce8317ee9719cfa04bb5f11d05e0a7e3f031
Tshepo <rocboyt@gmail.com>
2019-09-19 11:49:47 +0200

    Added key constants

--- app/src/main/java/co/za/dvt/myskilldevapp/constants/Constants.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/constants/Constants.kt
@@ -3,4 +3,8 @@ package co.za.dvt.myskilldevapp.constants
 val HOST = "http://appic.co.za/"
 const val GET_LUCKY_NUMBER = "api/luckyNumber.php"
 const val GET_AVAILABLE_CARS = "api/cars.php"
+
+const val TITLE = "tittle"
 const val ATMT = "attempt"
+const val LAYOUT = "layout"
+const val CATID = "catId"
