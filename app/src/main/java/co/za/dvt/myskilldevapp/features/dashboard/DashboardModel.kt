72ff1529cc2933fc9a59ffdaf55c6d9f701d0958
Tshepo <rocboyt@gmail.com>
2019-09-19 11:50:53 +0200

    Deleted dashboard model

--- app/src/main/java/co/za/dvt/myskilldevapp/features/dashboard/DashboardModel.kt
+++
@@ -1,9 +0,0 @@
-package co.za.dvt.myskilldevapp.features.dashboard
-
-import co.za.dvt.myskilldevapp.models.Car
-import co.za.dvt.myskilldevapp.models.LuckyNumberModel
-
-class DashboardModel {
-    var luckyNumberModel: LuckyNumberModel? = null
-    var availableCars: List<Car>? = null
-}
