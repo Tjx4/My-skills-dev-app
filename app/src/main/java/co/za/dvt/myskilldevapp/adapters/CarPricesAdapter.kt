cdea21322d0c9ea23dce9aee9c4743d7271c628d
Tshepo <rocboyt@gmail.com>
2019-09-20 10:58:48 +0200

    Added Car prices adapter

---
+++ app/src/main/java/co/za/dvt/myskilldevapp/adapters/CarPricesAdapter.kt
@@ -0,0 +1,76 @@
+package co.za.dvt.myskilldevapp.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import co.za.dvt.myskilldevapp.R
+import co.za.dvt.myskilldevapp.features.dashboard.DashboardActivity
+import co.za.dvt.myskilldevapp.models.Car
+import com.makeramen.roundedimageview.RoundedImageView
+
+
+class CarPricesAdapter(context: Context, private val cars: List<Car>) : RecyclerView.Adapter<CarPricesAdapter.ViewHolder>() {
+
+    private val dashboardActivity: DashboardActivity
+    private val mInflater: LayoutInflater
+    private var mClickListener: ItemClickListener? = null
+
+    init {
+        this.mInflater = LayoutInflater.from(context)
+        dashboardActivity = context as DashboardActivity
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = mInflater.inflate(R.layout.price_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val car = cars[position]
+        val model = car.model
+        val brand = car.brand
+        val color = car.color
+        val year = car.year
+        val pic = car.pic
+
+
+        holder.fullNameTv.text = model
+
+    }
+
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        internal var fullNameTv: TextView
+        internal var profPicImgv: RoundedImageView
+
+        init {
+            fullNameTv = itemView.findViewById(R.id.txtEmployeeName)
+            profPicImgv = itemView.findViewById(R.id.imgProfpic)
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View) {
+            if (mClickListener != null)
+                mClickListener!!.onItemClick(view, getAdapterPosition())
+        }
+    }
+
+    internal fun getItem(id: Int): String? {
+        return cars[id].model
+    }
+
+    fun setClickListener(itemClickListener: ItemClickListener) {
+        this.mClickListener = itemClickListener
+    }
+
+    interface ItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    override fun getItemCount(): Int {
+        return cars.size
+    }
+}
