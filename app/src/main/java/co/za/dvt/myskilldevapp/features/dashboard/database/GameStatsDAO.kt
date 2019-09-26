70bbb47761117de714d1454bc1462f5902bb2b72
Tshepo <rocboyt@gmail.com>
2019-09-26 08:22:38 +0200

    Added delete method

--- app/src/main/java/co/za/dvt/myskilldevapp/features/dashboard/database/GameStatsDAO.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/features/dashboard/database/GameStatsDAO.kt
@@ -1,10 +1,7 @@
 package co.za.dvt.myskilldevapp.features.dashboard.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface GameStatsDAO {
@@ -14,11 +11,15 @@ interface GameStatsDAO {
     @Update
     fun update(gameStats: GameStats)
 
-    @Query("SELECT * FROM game_stats_table ORDER BY game_id DESC")
-    fun getAllGameStats(): LiveData<List<GameStats>>
+    @Delete
+    fun delete(gameStats: GameStats)
 
     @Query("SELECT * FROM game_stats_table WHERE game_id = :key")
     fun get(key: Long):GameStats
+
+    @Query("SELECT * FROM game_stats_table ORDER BY game_id DESC")
+    fun getAllGameStats(): LiveData<List<GameStats>>
+
 
     @Query("DELETE  FROM game_stats_table")
     fun clear()
