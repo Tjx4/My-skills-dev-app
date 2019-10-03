435095cf05d57de02856df54f8fa6477e11a9d33
Tshepo <rocboyt@gmail.com>
2019-10-03 12:10:45 +0200

    Added luckyNumber test

--- app/src/test/java/co/za/dvt/myskilldevapp/DashboardUnitTest.kt
+++ app/src/test/java/co/za/dvt/myskilldevapp/DashboardUnitTest.kt
@@ -3,8 +3,8 @@ package co.za.dvt.myskilldevapp
 import co.za.dvt.myskilldevapp.features.dashboard.DashboardViewModel
 import io.mockk.mockk
 import org.junit.Test
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.TestInstance
-
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DashboardUnitTest {
@@ -12,8 +12,17 @@ class DashboardUnitTest {
     private val dashboardViewModel: DashboardViewModel = mockk()
 
     @Test
-    fun isLuckyNumner_set_correctly() {
+    fun isLuckyNumber_set_correctly() {
+        // given
+        // dashboardViewModel.luckyNumber.value = 0
 
+        // when
+        dashboardViewModel.setLuckyNumber()
+        val actualResult = dashboardViewModel.luckyNumber.value?.luckyNumber != 0 && dashboardViewModel.isError.value == false
+        val expectedResult = true
+
+        // then
+        Assertions.assertEquals(expectedResult, actualResult)
     }
 
 }
