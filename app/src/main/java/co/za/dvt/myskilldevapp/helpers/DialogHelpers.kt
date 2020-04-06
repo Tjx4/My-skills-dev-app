9513333c7bb22300f0717bd2198054d33af75d5d
Tshepo <rocboyt@gmail.com>
2020-04-06 11:44:18 +0200

    Created dice game

--- app/src/main/java/co/za/dvt/myskilldevapp/helpers/DialogHelpers.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/helpers/DialogHelpers.kt
@@ -7,12 +7,10 @@ import co.za.dvt.myskilldevapp.features.activities.BaseActivity
 import co.za.dvt.myskilldevapp.features.fragments.BaseDialogFragment
 import co.za.dvt.myskilldevapp.features.fragments.LoadingSpinnerFragment
 
-
 fun showLoadingDialog(loadingMessage: String, activity: BaseActivity) {
     var loadingSpinnerFragment = LoadingSpinnerFragment.newInstance("")
     showDialogFragment(loadingMessage, R.layout.fragment_loading_spinner, loadingSpinnerFragment, activity)
     loadingSpinnerFragment.isCancelable = false
-    activity.activeDialogFragment = loadingSpinnerFragment
 }
 
 fun hideCurrentLoadingDialog(activity: BaseActivity) {
@@ -20,9 +18,11 @@ fun hideCurrentLoadingDialog(activity: BaseActivity) {
 }
 
 fun showDialogFragment(title: String, Layout: Int, newFragmentBaseBase: BaseDialogFragment, activity: BaseActivity) {
+    activity?.activeDialogFragment?.dismiss()
     val ft = activity.supportFragmentManager.beginTransaction()
     var newFragment = getFragmentDialog(title, Layout, newFragmentBaseBase)
     newFragment.show(ft, "dialog")
+    activity.activeDialogFragment = newFragment
 }
 
 private fun getFragmentDialog(title: String, Layout: Int, newFragmentBaseBase: BaseDialogFragment) : BaseDialogFragment {
