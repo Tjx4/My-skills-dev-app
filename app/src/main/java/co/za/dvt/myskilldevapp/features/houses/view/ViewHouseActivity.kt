d644df339c0b8c7b6295e5a45bdf1895cfe7b7f9
Tshepo <rocboyt@gmail.com>
2020-05-11 19:22:54 +0200

    view house fix

--- app/src/main/java/co/za/dvt/myskilldevapp/features/houses/view/ViewHouseActivity.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/features/houses/view/ViewHouseActivity.kt
@@ -9,7 +9,7 @@ import co.za.dvt.myskilldevapp.R
 import co.za.dvt.myskilldevapp.adapters.CharactersAdapter
 import co.za.dvt.myskilldevapp.constants.HOUSE
 import co.za.dvt.myskilldevapp.constants.PAYLOAD_KEY
-import co.za.dvt.myskilldevapp.databinding.ActivityHousesBinding
+import co.za.dvt.myskilldevapp.databinding.ActivityHouseBinding
 import co.za.dvt.myskilldevapp.features.activities.BaseChildActivity
 import co.za.dvt.myskilldevapp.features.characters.CharatersRepository
 import co.za.dvt.myskilldevapp.models.House
@@ -17,19 +17,14 @@ import co.za.dvt.myskilldevapp.models.Character
 import kotlinx.android.synthetic.main.activity_house.*
 
 class ViewHouseActivity : BaseChildActivity() {
-    private lateinit var binding: ActivityViewHouseBinding
+    private lateinit var binding: ActivityHouseBinding
     private lateinit var viewHouseViewModel: ViewHouseViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val house = intent.getBundleExtra(PAYLOAD_KEY)?.getParcelable(HOUSE) ?: House()  //Todo: Fix
-        supportActionBar?.title = house?.name
-        txtHouseMascot.text = house?.mascot
-        //txtHeadOfHouse.text = house?.headOfHouse
-        txtHouseFounder.text = house?.founder
-        txtHouseGhost.text = house?.houseGhost
-        
+
         var charatersRepository = CharatersRepository()
         var application = requireNotNull(this).application
         var viewModelFactory = ViewHouseModelFactory(house, charatersRepository, application)
@@ -39,13 +34,20 @@ class ViewHouseActivity : BaseChildActivity() {
         binding.viewHouseViewModel = viewHouseViewModel
         binding.lifecycleOwner = this
 
+        supportActionBar?.title = house?.name
+        txtHouseMascot.text = house?.mascot
+        //txtHeadOfHouse.text = house?.headOfHouse
+        txtHouseFounder.text = house?.founder
+        txtHouseGhost.text = house?.houseGhost
+
         viewHouseViewModel.members.observe(this, Observer { showMembers(it) })
+
+        viewHouseViewModel.getAndShowHouses()
     }
 
     fun showMembers(members: List<Character?>?){
         rvMembers?.layoutManager = LinearLayoutManager(this)
-        val charactersAdapter = CharactersAdapter(this, R.layout.character_layout, members)
-        //charactersAdapter.setClickListener(this)
+        val charactersAdapter = CharactersAdapter(this, R.layout.small_character_layout, members)
         rvMembers?.adapter = charactersAdapter
     }
 }
