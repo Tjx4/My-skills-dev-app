c46c79d078e92bc607e30bafacbec83711f83bad
Tshepo <rocboyt@gmail.com>
2020-05-11 14:17:01 +0200

    Added view spell

--- app/src/main/java/co/za/dvt/myskilldevapp/features/characters/CharatersActivity.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/features/characters/CharatersActivity.kt
@@ -9,20 +9,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import co.za.dvt.myskilldevapp.R
 import co.za.dvt.myskilldevapp.adapters.CharactersAdapter
 import co.za.dvt.myskilldevapp.constants.CHARACTER
-import co.za.dvt.myskilldevapp.constants.HOUSE
-import co.za.dvt.myskilldevapp.constants.PAYLOAD_KEY
-import co.za.dvt.myskilldevapp.databinding.ActivityStudentsBinding
+import co.za.dvt.myskilldevapp.databinding.ActivityCharactersBinding
 import co.za.dvt.myskilldevapp.extensions.SLIDE_IN_ACTIVITY
 import co.za.dvt.myskilldevapp.extensions.goToActivityWithPayload
 import co.za.dvt.myskilldevapp.features.activities.BaseChildActivity
 import co.za.dvt.myskilldevapp.helpers.hideCurrentLoadingDialog
 import co.za.dvt.myskilldevapp.helpers.showLoadingDialog
 import co.za.dvt.myskilldevapp.models.Character
-import co.za.dvt.myskilldevapp.models.House
-import kotlinx.android.synthetic.main.activity_students.*
+import kotlinx.android.synthetic.main.activity_characters.*
 
 class CharatersActivity : BaseChildActivity(), CharactersAdapter.CharacterClickListener {
-    private lateinit var binding: ActivityStudentsBinding
+    private lateinit var binding: ActivityCharactersBinding
     private lateinit var charactersViewModel: CharatersViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +32,7 @@ class CharatersActivity : BaseChildActivity(), CharactersAdapter.CharacterClickL
         var viewModelFactory = CharatersViewModelFactory(studentsRepository, application)
 
         charactersViewModel = ViewModelProviders.of(this, viewModelFactory).get(CharatersViewModel::class.java)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_students)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_characters)
         binding.studentsViewModel = charactersViewModel
         binding.lifecycleOwner = this
 
--- app/src/main/java/co/za/dvt/myskilldevapp/features/spells/SpellActivity.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/features/spells/SpellActivity.kt
@@ -6,11 +6,13 @@ import co.za.dvt.myskilldevapp.R
 import co.za.dvt.myskilldevapp.constants.HOUSE
 import co.za.dvt.myskilldevapp.constants.PAYLOAD_KEY
 import co.za.dvt.myskilldevapp.constants.SPELL
+import co.za.dvt.myskilldevapp.features.activities.BaseChildActivity
 import co.za.dvt.myskilldevapp.models.House
 import co.za.dvt.myskilldevapp.models.Spell
 import kotlinx.android.synthetic.main.activity_house.*
+import kotlinx.android.synthetic.main.activity_spell.*
 
-class SpellActivity : AppCompatActivity() {
+class SpellActivity : BaseChildActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +20,7 @@ class SpellActivity : AppCompatActivity() {
 
         val spell = intent.getBundleExtra(PAYLOAD_KEY)?.getParcelable<Spell>(SPELL)
         supportActionBar?.title = spell?.spell
-        //txtHouseMascot.text = "Mascot: ${spell?.effect}"
-        //txtHeadOfHouse.text = "Head of house: ${spell?.type}"
+        tvSpellEffect.text = "Effect: ${spell?.effect}"
+        tvSpellType.text = "Type: ${spell?.type}"
     }
 }
