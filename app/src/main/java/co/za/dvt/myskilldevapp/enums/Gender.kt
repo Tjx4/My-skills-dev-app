b7c256ca14ae0421517de5c54d5a23510f0a2ede
Tshepo <rocboyt@gmail.com>
2020-08-26 18:15:00 +0200

    Added newtral gender

--- app/src/main/java/co/za/dvt/myskilldevapp/enums/Gender.kt
+++ app/src/main/java/co/za/dvt/myskilldevapp/enums/Gender.kt
@@ -1,6 +1,7 @@
 package co.za.dvt.myskilldevapp.enums
 
 enum class Gender constructor(var id: Int, var sex: String, var genderTitle: String, var genderPronoun: String) {
+    Neutral(0, "Neutral", "Neutral", "Neutral"),
     Male(1, "Male", "Man", "He"),
     Female(2, "Female", "Woman", "She")
 }
